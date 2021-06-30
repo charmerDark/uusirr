84fdb5ef073df59c5fa2a6f2511fbee46e8da6eb
vishnu <vishnuajith@gmail.com>
2021-06-30 21:33:28 +0530

    adding __init__ to datasets

---
+++ datasets/__init__.py
@@ -0,0 +1,21 @@
+from . import sintel
+
+
+
+
+## Sintel
+SintelTestClean = sintel.SintelTestClean
+SintelTestFinal = sintel.SintelTestFinal
+
+SintelTrainingCombFull = sintel.SintelTrainingCombFull
+SintelTrainingCombTrain = sintel.SintelTrainingCombTrain
+SintelTrainingCombValid = sintel.SintelTrainingCombValid
+
+SintelTrainingCleanFull = sintel.SintelTrainingCleanFull
+SintelTrainingCleanTrain = sintel.SintelTrainingCleanTrain
+SintelTrainingCleanValid = sintel.SintelTrainingCleanValid
+
+SintelTrainingFinalFull = sintel.SintelTrainingFinalFull
+SintelTrainingFinalTrain = sintel.SintelTrainingFinalTrain
+SintelTrainingFinalValid = sintel.SintelTrainingFinalValid
+
