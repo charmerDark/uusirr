7c7098bf9992bad203c696a8a959a19b644a96b5
charmerDark <vishnuajith@gmail.com>
2025-03-24 16:04:17 +0000

    adding baseline models

---
+++ scripts/train_dual_classifier.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=Teach-LongJobs
+#SBATCH --gres=gpu:1
+#SBATCH --mem=12000  # memory in Mb
+
+
+export STUDENT_ID=$(whoami)
+source /home/${STUDENT_ID}/miniconda3/bin/activate uusirr
+
+python /home/${STUDENT_ID}/uusirr/dual_image_classifier_training.py
